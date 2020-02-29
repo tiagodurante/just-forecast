ce38b38c3a2462091688d2465e1545460662c17b
Tiago Amado Durante <tiagodurante@icloud.com>
2020-02-29 00:34:27 -0300

    removendo arquivos desnecessários

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,11 @@ module.exports = {
     themeColor: '#f3d4d4',
     msTileColor: '#f3d4d4',
     appleMobileWebAppStatusBarStyle: '#f3d4d4',
+    appleMobileWebAppCapable: 'yes',
     name: 'Just Forecast',
-    iconPaths: [
-      {
-        src: 'manifest-icon-192.png',
-        sizes: '192x192',
-        type: 'image/png'
-      },
-      {
-        src: 'manifest-icon-512.png',
-        sizes: '512x512',
-        type: 'image/png'
-      }
-    ]
+    manifestOptions: {
+      background_color: '#f3d4d4'
+    }
   },
   transpileDependencies: [
     'vuetify'
