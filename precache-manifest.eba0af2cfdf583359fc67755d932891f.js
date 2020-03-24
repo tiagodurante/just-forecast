a676175e9b5b904ccfa2eb9f44e485a51ea1c416
Tiago Amado Durante <tiagodurante@icloud.com>
2020-03-23 22:05:05 -0300

    deploy

---
+++ precache-manifest.eba0af2cfdf583359fc67755d932891f.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c558c8f2d96ed8647ddd",
+    "url": "/just-forecast/css/app.567c50d5.css"
+  },
+  {
+    "revision": "eafdce8ce3b766a291ad",
+    "url": "/just-forecast/css/chunk-vendors.f1c8a128.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/just-forecast/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/just-forecast/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/just-forecast/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/just-forecast/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "313a65630d341645c13e4f2a0364381d",
+    "url": "/just-forecast/fonts/Roboto-Black.313a6563.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/just-forecast/fonts/Roboto-Black.59eb3601.woff2"
+  },
+  {
+    "revision": "cc2fadc3928f2f223418887111947b40",
+    "url": "/just-forecast/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "f75569f8a5fab0893fa712d8c0d9c3fe",
+    "url": "/just-forecast/fonts/Roboto-BlackItalic.f75569f8.woff2"
+  },
+  {
+    "revision": "50d75e48e0a3ddab1dd15d6bfb9d3700",
+    "url": "/just-forecast/fonts/Roboto-Bold.50d75e48.woff"
+  },
+  {
+    "revision": "b52fac2bb93c5858f3f2675e4b52e1de",
+    "url": "/just-forecast/fonts/Roboto-Bold.b52fac2b.woff2"
+  },
+  {
+    "revision": "4fe0f73cc919ba2b7a3c36e4540d725c",
+    "url": "/just-forecast/fonts/Roboto-BoldItalic.4fe0f73c.woff"
+  },
+  {
+    "revision": "94008e69aaf05da75c0bbf8f8bb0db41",
+    "url": "/just-forecast/fonts/Roboto-BoldItalic.94008e69.woff2"
+  },
+  {
+    "revision": "c73eb1ceba3321a80a0aff13ad373cb4",
+    "url": "/just-forecast/fonts/Roboto-Light.c73eb1ce.woff"
+  },
+  {
+    "revision": "d26871e8149b5759f814fd3c7a4f784b",
+    "url": "/just-forecast/fonts/Roboto-Light.d26871e8.woff2"
+  },
+  {
+    "revision": "13efe6cbc10b97144a28310ebdeda594",
+    "url": "/just-forecast/fonts/Roboto-LightItalic.13efe6cb.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/just-forecast/fonts/Roboto-LightItalic.e8eaae90.woff2"
+  },
+  {
+    "revision": "1d6594826615607f6dc860bb49258acb",
+    "url": "/just-forecast/fonts/Roboto-Medium.1d659482.woff"
+  },
+  {
+    "revision": "90d1676003d9c28c04994c18bfd8b558",
+    "url": "/just-forecast/fonts/Roboto-Medium.90d16760.woff2"
+  },
+  {
+    "revision": "13ec0eb5bdb821ff4930237d7c9f943f",
+    "url": "/just-forecast/fonts/Roboto-MediumItalic.13ec0eb5.woff2"
+  },
+  {
+    "revision": "83e114c316fcc3f23f524ec3e1c65984",
+    "url": "/just-forecast/fonts/Roboto-MediumItalic.83e114c3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/just-forecast/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "73f0a88bbca1bec19fb1303c689d04c6",
+    "url": "/just-forecast/fonts/Roboto-Regular.73f0a88b.woff2"
+  },
+  {
+    "revision": "4357beb823a5f8d65c260f045d9e019a",
+    "url": "/just-forecast/fonts/Roboto-RegularItalic.4357beb8.woff2"
+  },
+  {
+    "revision": "f5902d5ef961717ed263902fc429e6ae",
+    "url": "/just-forecast/fonts/Roboto-RegularItalic.f5902d5e.woff"
+  },
+  {
+    "revision": "ad538a69b0e8615ed0419c4529344ffc",
+    "url": "/just-forecast/fonts/Roboto-Thin.ad538a69.woff2"
+  },
+  {
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/just-forecast/fonts/Roboto-Thin.d3b47375.woff"
+  },
+  {
+    "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
+    "url": "/just-forecast/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
+  },
+  {
+    "revision": "8a96edbbcd9a6991d79371aed0b0288e",
+    "url": "/just-forecast/fonts/Roboto-ThinItalic.8a96edbb.woff"
+  },
+  {
+    "revision": "a842012aa77dec99f000668f8ca8c241",
+    "url": "/just-forecast/img/1.a842012a.png"
+  },
+  {
+    "revision": "610765eb644349283d1ed19f30cad11d",
+    "url": "/just-forecast/img/11.610765eb.png"
+  },
+  {
+    "revision": "41ebdf2ddff4fc07827797ecc120ce61",
+    "url": "/just-forecast/img/12.41ebdf2d.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/13.aaefa0e0.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/14.aaefa0e0.png"
+  },
+  {
+    "revision": "6067c3b0257be693328f80301713c0a2",
+    "url": "/just-forecast/img/15.6067c3b0.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/16.aaefa0e0.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/17.aaefa0e0.png"
+  },
+  {
+    "revision": "e9948dbffcc87479afef42ea1336cc16",
+    "url": "/just-forecast/img/18.e9948dbf.png"
+  },
+  {
+    "revision": "afed36278400d3f0900ee004b8b7ee74",
+    "url": "/just-forecast/img/19.afed3627.png"
+  },
+  {
+    "revision": "482b7a05dd5c3571ef37c354d393a7ba",
+    "url": "/just-forecast/img/2.482b7a05.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/20.aaefa0e0.png"
+  },
+  {
+    "revision": "aaefa0e0487c54ac8afba48367f6e39a",
+    "url": "/just-forecast/img/21.aaefa0e0.png"
+  },
+  {
+    "revision": "bf6dd208182c6ee98bcc976251082cbb",
+    "url": "/just-forecast/img/22.bf6dd208.png"
+  },
+  {
+    "revision": "2ae2a19fec4f2d5b29fe66ba1f815e9b",
+    "url": "/just-forecast/img/23.2ae2a19f.png"
+  },
+  {
+    "revision": "24fe18b9807578d06bc4d8d603d7e8f7",
+    "url": "/just-forecast/img/24.24fe18b9.png"
+  },
+  {
+    "revision": "92327f0a41237b6d10b4be185c7f35bc",
+    "url": "/just-forecast/img/25.92327f0a.png"
+  },
+  {
+    "revision": "efa0e8830b7d862a949c2e673740d8df",
+    "url": "/just-forecast/img/26.efa0e883.png"
+  },
+  {
+    "revision": "efa0e8830b7d862a949c2e673740d8df",
+    "url": "/just-forecast/img/29.efa0e883.png"
+  },
+  {
+    "revision": "9c240d4aa886efacb57fd1c0ff99dffd",
+    "url": "/just-forecast/img/3.9c240d4a.png"
+  },
+  {
+    "revision": "4d8b8da98b820705320b67d30018bb09",
+    "url": "/just-forecast/img/30.4d8b8da9.png"
+  },
+  {
+    "revision": "700a3495a1ef8d98e3b1d16a22dc888b",
+    "url": "/just-forecast/img/31.700a3495.png"
+  },
+  {
+    "revision": "233d228b1e09365698219faf715562ae",
+    "url": "/just-forecast/img/32.233d228b.png"
+  },
+  {
+    "revision": "6a847b7d171d0334191085ef13a5fca5",
+    "url": "/just-forecast/img/33.6a847b7d.png"
+  },
+  {
+    "revision": "3389ba871e0ee31ed5a7427c8491f8a9",
+    "url": "/just-forecast/img/34.3389ba87.png"
+  },
+  {
+    "revision": "baed0d7f6e3a3f18fce07ce13e64e11b",
+    "url": "/just-forecast/img/35.baed0d7f.png"
+  },
+  {
+    "revision": "baed0d7f6e3a3f18fce07ce13e64e11b",
+    "url": "/just-forecast/img/36.baed0d7f.png"
+  },
+  {
+    "revision": "e7b9e58f6757a282bb7183658e7657aa",
+    "url": "/just-forecast/img/37.e7b9e58f.png"
+  },
+  {
+    "revision": "e7b9e58f6757a282bb7183658e7657aa",
+    "url": "/just-forecast/img/38.e7b9e58f.png"
+  },
+  {
+    "revision": "afed36278400d3f0900ee004b8b7ee74",
+    "url": "/just-forecast/img/39.afed3627.png"
+  },
+  {
+    "revision": "5463d62c4a4ca3e774d05f235606844a",
+    "url": "/just-forecast/img/4.5463d62c.png"
+  },
+  {
+    "revision": "afed36278400d3f0900ee004b8b7ee74",
+    "url": "/just-forecast/img/40.afed3627.png"
+  },
+  {
+    "revision": "6067c3b0257be693328f80301713c0a2",
+    "url": "/just-forecast/img/42.6067c3b0.png"
+  },
+  {
+    "revision": "efa0e8830b7d862a949c2e673740d8df",
+    "url": "/just-forecast/img/43.efa0e883.png"
+  },
+  {
+    "revision": "bf6dd208182c6ee98bcc976251082cbb",
+    "url": "/just-forecast/img/44.bf6dd208.png"
+  },
+  {
+    "revision": "7e0d3e4f595c1b4dbdc57099558bbbf4",
+    "url": "/just-forecast/img/5.7e0d3e4f.png"
+  },
+  {
+    "revision": "9c240d4aa886efacb57fd1c0ff99dffd",
+    "url": "/just-forecast/img/6.9c240d4a.png"
+  },
+  {
+    "revision": "210963a8d36e3bdcc3b20ca4c2405c0a",
+    "url": "/just-forecast/img/7.210963a8.png"
+  },
+  {
+    "revision": "f7c4f85ecb8be6b33fca0ccbc08af245",
+    "url": "/just-forecast/img/8.f7c4f85e.png"
+  },
+  {
+    "revision": "4e114b2081f55c66e45e0bdf9acd0755",
+    "url": "/just-forecast/img/file.4e114b20.png"
+  },
+  {
+    "revision": "84d3d814247b0e151f79d27435ed81f8",
+    "url": "/just-forecast/index.html"
+  },
+  {
+    "revision": "c558c8f2d96ed8647ddd",
+    "url": "/just-forecast/js/app.49dcdb58.js"
+  },
+  {
+    "revision": "eafdce8ce3b766a291ad",
+    "url": "/just-forecast/js/chunk-vendors.b1935d14.js"
+  },
+  {
+    "revision": "2da215f365ceb6fa3a37b9f57f4907ca",
+    "url": "/just-forecast/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/just-forecast/robots.txt"
+  }
+]);
